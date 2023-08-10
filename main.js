5a75023ca201915071d25375dc97237bcb7fdd73
Alemba-Vivian <alembavivian@gmail.com>
2023-08-10 16:20:21 +0300

    added files

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+// setting up Gameboard object module
+const Gameboard =()=>(()=>{
+
+    let gameboard =["", "","", "","","","","",""];
+
+
+
+})();
+
+
+//setting up the DisplayController object module
+const DisplayController =()=>(()=>{
+
+})();
+
+//create multiple players using functory functions
+const createPlayers =()=>{
+
+}
